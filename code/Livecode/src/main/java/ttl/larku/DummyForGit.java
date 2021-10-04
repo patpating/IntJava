730cde822237b7abdb8861c503b9d07f6b8e20f5
whynot <anil@panix.com>
2021-10-04 17:01:58 -0400

    Initial Load

---
+++ code/Livecode/src/main/java/ttl/larku/DummyForGit.java
@@ -0,0 +1,10 @@
+
+/**
+ * @author whynot
+ */
+public class DummyForGit {
+
+    public static void main(String[] args) {
+        System.out.println("Nothing here, just a dummy for git");
+    }
+}
---
+++ code/StudentWork/src/main/java/studentwork/dummy/DummyForGit.java
@@ -0,0 +1,10 @@
+package studentwork.dummy;
+/**
+ * @author whynot
+ */
+public class DummyForGit {
+
+    public static void main(String[] args) {
+        System.out.println("Dummy for git.  You can delete file this when you have other code in this module");
+    }
+}
