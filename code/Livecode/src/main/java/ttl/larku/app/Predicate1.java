da3058a01cd3239585cea004df576b8d2212689f
whynot <anil@panix.com>
2021-10-07 14:20:01 -0400

    Predicate 2

--- code/Livecode/src/main/java/ttl/larku/app/Predicate1.java
+++ code/Livecode/src/main/java/ttl/larku/app/Predicate1.java
@@ -5,11 +5,13 @@ import ttl.larku.domain.Student;
 import ttl.larku.service.StudentService;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * @author whynot
@@ -20,7 +22,8 @@ public class Predicate1 {
     public static void main(String[] args) {
         Predicate1 ra = new Predicate1();
 //        ra.filtering();
-        ra.filtering2();
+        //ra.filtering2();
+        ra.filtering3();
 
     }
 
@@ -42,13 +45,86 @@ public class Predicate1 {
 
         List<Student> r1 = slightlyBetterFilter(students, s -> s.getName().startsWith("M"));
 
-        List<Student> r2 = slightlyBetterFilter(students, s -> s.getOtherProp().endsWith("X"));
+//        List<Student> r2 = slightlyBetterFilter(students, s -> s.getOtherProp().endsWith("X"));
 
-        r1.forEach(System.out::println);
+        List<Student> r3 = slightlyBetterFilter(students, s -> s.getDob().until(LocalDate.now(), ChronoUnit.YEARS) > 50);
+
+        r3.forEach(System.out::println);
     }
+
+    public void filtering3() {
+        StudentService service = TheFactory.studentService();
+        initStudentService(service);
+        List<Student> students = service.getAllStudents();
+
+
+        List<Student> r1 = moreBetterFilter(students, s -> s.getName().startsWith("M"));
+
+//        List<Student> r2 = slightlyBetterFilter(students, s -> s.getOtherProp().endsWith("X"));
+
+        List<Student> r3 = moreBetterFilter(students, s -> s.getDob().until(LocalDate.now(), ChronoUnit.YEARS) > 50);
+
+        List<String> strings = List.of("one", "two", "Threeeee");
+        List<String> r4 = moreBetterFilter(strings, s -> s.length() > 4);
+
+        r4.forEach(System.out::println);
+    }
+
+    public void filtering4() {
+        StudentService service = TheFactory.studentService();
+        initStudentService(service);
+        List<Student> students = service.getAllStudents();
+
+
+        List<Student> r1 = bestFilter(students, s -> s.getName().startsWith("M"));
+
+//        List<Student> r2 = slightlyBetterFilter(students, s -> s.getOtherProp().endsWith("X"));
+
+        List<Student> r3 = bestFilter(students, s -> s.getDob().until(LocalDate.now(), ChronoUnit.YEARS) > 50);
+
+        List<String> strings = List.of("one", "two", "Threeeee");
+        List<String> r4 = bestFilter(strings, s -> s.length() > 4);
+
+        r4.forEach(System.out::println);
+    }
+
+    public <T> List<T> bestFilter(List<T> input, Predicate<T> checker) {
+        List<T> result = new ArrayList<>();
+        for(T s : input) {
+            if(checker.test(s)) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
 
     interface Checker {
         public boolean check(Student s);
+    }
+
+    interface GenericChecker<X> {
+        public boolean check(X s);
+    }
+
+    public <T> List<T> moreBetterFilter(List<T> input, GenericChecker<T> checker) {
+        List<T> result = new ArrayList<>();
+        for(T s : input) {
+            if(checker.check(s)) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
+    public List<Student> moreBetterFilter0(List<Student> input, GenericChecker<Student> checker) {
+        List<Student> result = new ArrayList<>();
+        for(Student s : input) {
+            if(checker.check(s)) {
+                result.add(s);
+            }
+        }
+        return result;
     }
 
     public List<Student> slightlyBetterFilter(List<Student> input, Checker checker) {
@@ -61,6 +137,12 @@ public class Predicate1 {
         return result;
     }
 
+    class NameGenericChecker implements GenericChecker<Student> {
+        @Override
+        public boolean check(Student s) {
+            return s.getName().startsWith("M");
+        }
+    }
     class NameChecker implements Checker {
         @Override
         public boolean check(Student s) {
